9de7d7ae9a2dbd6b41d776ce78f916fa127823af
khushijaiswal <jaiswalkhushi9011@gmail.com>
2025-05-01 08:41:32 +0530

    update rdis client file

--- server/redisClient.js
+++ server/redisClient.js
@@ -1,7 +1,7 @@
 const redis = require('redis');
 
 const redisClient = redis.createClient({
-    url: process.env.REDIS_URL,
+    url: process.env.REDIS_HOST,
 });
 
 redisClient.on('error', (err) => console.log('Redis Client Error', err));
